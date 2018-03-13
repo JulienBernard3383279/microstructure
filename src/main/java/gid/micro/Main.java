baf157d5a8a2211b4191f706d684a3012a90e563
bernajul <jul.bern@free.fr>
2018-03-13 17:13:53 +0100

    yoann wat r u doin

--- src/main/java/gid/micro/Main.java
+++ src/main/java/gid/micro/Main.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Data> dataList = Parser.parseData("../data/data.csv");
+        List<Data> dataList = Parser.parseData("./data/data.csv");
         System.out.println(dataList.get(0));
-        Correlation corr = new Correlation(dataList);
-        System.out.println(corr.toLatex());
+        //Correlation corr = new Correlation(dataList);
+        //System.out.println(corr.toLatex());
     }
 }
