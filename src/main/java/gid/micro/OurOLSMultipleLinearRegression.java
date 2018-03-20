5ecdabd3fe2332eefb2afa4f49c792d54502fa97
bernajul <jul.bern@free.fr>
2018-03-20 11:47:53 +0100

    Ah je suis en PEPS je peux faire des messages de commit qui parlent de tapis

---
+++ src/main/java/gid/micro/OurOLSMultipleLinearRegression.java
@@ -0,0 +1,22 @@
+package gid.micro;
+
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
+
+public class OurOLSMultipleLinearRegression extends OLSMultipleLinearRegression {
+	public double[] ourCalculateBeta() {
+		RealVector result = calculateBeta();
+		double[] array = new double[result.getDimension()];
+		for (int i=0; i<result.getDimension(); i++) {
+			array[i]=result.getEntry(i);
+		}
+		return array;
+		
+	}
+	
+	public double[][] ourCalculateBetaVariance() {
+		RealMatrix result = calculateBetaVariance();
+		return result.getData();
+	}
+}
